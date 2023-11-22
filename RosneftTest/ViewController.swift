3e5ccc49df7868f78ca217255dcbfd9202f3f8c3
RRDubina <dubinaroman.dubina@gmail.com>
2023-11-22 17:11:59 +0500

    Initial Commit

---
+++ RosneftTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RosneftTest
+//
+//  Created by admin on 22.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
