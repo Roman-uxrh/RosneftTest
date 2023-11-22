c7776f1fe032b6fe77e33723466d78a3503f9e39
RRDubina <dubinaroman.dubina@gmail.com>
2023-11-22 18:47:58 +0500

    Delete storyBoard, fixed the navigation of the project

---
+++ RosneftTest/Sourse/App/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  RosneftTest
+//
+//  Created by admin on 22.11.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
